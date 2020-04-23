ac599336e196e518a08c99aa79bffc04eea67a42
Gašper Mlakar <spagr88@gmail.com>
2020-04-23 21:11:46 +0200

    add number guess

--- guess_the_secret_number.py
+++ guess_the_secret_number.py
@@ -5,4 +5,4 @@ guess = int(input("What is my secret number? (between 1 and 20) "))
 if guess == SECRET:
     print("YEEEEEEE, you find my number!")
 else:
-    print("Wrong, choose another number")
+    print("Wrong, choose another number... The secret number is not " + str(guess))
