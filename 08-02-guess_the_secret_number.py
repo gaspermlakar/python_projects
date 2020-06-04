59a3b2bc44f5e247e3225bf303eabb87523775ba
Gašper Mlakar <spagr88@gmail.com>
2020-06-04 16:43:44 +0200

    import random secret number, add read and write to file

--- 08-02-guess_the_secret_number.py
+++ 08-02-guess_the_secret_number.py
@@ -1,8 +1,26 @@
-SECRET = 8
+import random
 
-guess = int(input("What is my secret number? (between 1 and 20) "))
+secret = random.randint(1,30)
+attempts = 0
 
-if guess == SECRET:
-    print("YEEEEEEE, you find my number!")
-else:
-    print("Wrong, choose another number... The secret number is not " + str(guess))
+with open("08-02-score.txt", "r") as score_file:
+    best_score = int(score_file.read())
+    print("Top score: " + str(best_score))
+
+while True:
+    guess = int(input("What is my secret number? (between 1 and 30) "))
+    attempts += 1
+
+    if guess == secret:
+        if attempts < best_score:
+            with open("08-02-score.txt", "w") as score_file:
+                score_file.write(str(attempts))
+        print("YEEEEEEE, you guessed it! It's number " + str(secret))
+        print("Attempts nedded: " + str(attempts))
+        break
+
+    if guess > secret:
+         print("Your guess is wrong... try something smaller ")
+
+    elif guess < secret:
+        print("Your guess is wrong... try something bigger ")
